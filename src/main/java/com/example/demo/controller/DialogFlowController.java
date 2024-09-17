8681a0970902b71a131390b95e04415e153e8d8e
Jeswin Sam <jeswinsam.sundar@gmail.com>
2024-09-18 00:45:53 +0530

    controller will return json object

--- src/main/java/com/example/demo/controller/DialogFlowController.java
+++ src/main/java/com/example/demo/controller/DialogFlowController.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.MenuItemRepository;
 import com.example.demo.service.DialogFlowService;
 import com.example.demo.service.MenuItemService;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class DialogFlowController {
     DialogFlowService dialogFlowService;
 
     @PostMapping("/dialogflow")
-    public void processRequest(@RequestBody String jsonInput){
+    public JSONObject processRequest(@RequestBody String jsonInput){
 
         String intent = getIntent(jsonInput);
         Long orderId = getOrderId(jsonInput);
@@ -32,27 +33,31 @@ public class DialogFlowController {
         List<String> foodItems = getFoodItems(jsonInput);
         List<Integer> numbers = getNumbers(jsonInput);
 
+        JSONObject jsonObject = new JSONObject();
+
         switch (intent) {
 
             case "New Order Intent": {
                 dialogFlowService.newOrder();
-                break;
+                return jsonObject;
             }
             case "Add to order Intent": {
-               dialogFlowService.addToOrder(foodItems, numbers);
-                break;
+                jsonObject.put("fulfillmentText", dialogFlowService.addToOrder(foodItems, numbers));
+                return jsonObject;
             }
             case "Remove from order Intent": {
-                dialogFlowService.removeFromOrder(foodItems,numbers);
-                break;
+                jsonObject.put("fulfillmentText", dialogFlowService.removeFromOrder(foodItems,numbers));
+                return jsonObject;
             }
             case "Order Complete Intent":{
-                dialogFlowService.finishOrder();
-                break;
+                jsonObject.put("fulfillmentText", dialogFlowService.finishOrder());
+                return jsonObject;
             }
             case "Track order with order number": {
-                dialogFlowService.getOrderStatusById(orderId);
+                jsonObject.put("fulfillmentText", dialogFlowService.getOrderStatusById(orderId));
+                return jsonObject;
             }
         }
+        return jsonObject;
     }
 }
